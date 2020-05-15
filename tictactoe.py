5a25521777579e30109cb5fd09c3817539edfb7a
Rebecca Ceppas <receppasdc@gmail.com>
2020-05-15 14:45:52 -0400

    created file

---
+++ tictactoe.py
@@ -0,0 +1,25 @@
+def sampleboard():
+    
+    #prints options for user
+
+    board = '\n _|_|_\n _|_|_\n _|_|_\n'
+    print(board)
+
+
+
+def game():
+
+    #put everything together
+
+    pass
+
+start = input('Do you want to play tictactoe? (Y/N)\n')
+if start.upper() == 'Y':
+    print('You are are the X')
+    sampleboard()
+
+
+
+
+
+
